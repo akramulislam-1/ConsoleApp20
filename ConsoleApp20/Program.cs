32845515bf03a0da8dc39e730867a187c3dd76c7
akram <akram29031999@gmail.com>
2022-09-20 07:45:40 +0530

    Add project files.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
